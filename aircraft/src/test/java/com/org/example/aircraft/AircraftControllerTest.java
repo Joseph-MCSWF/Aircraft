74af5e51bd5cd0491ce615351afef830c840d0ae
Joseph Minton <joseph.minton@usmc.mil>
2025-09-05 10:02:51 -0500

    add gitignore and start test

---
+++ aircraft/src/test/java/com/org/example/aircraft/AircraftControllerTest.java
@@ -0,0 +1,13 @@
+package com.org.example.aircraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AircraftControllerTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
